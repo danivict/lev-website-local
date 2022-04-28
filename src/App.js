ca2a93cb02fe2cc128a21e4c4b2751709711865e
Caio Mendes <cacammoreno@gmail.com>
2022-04-28 19:52:11 -0300

    feat: home - footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
 import Home from './pages/Home';
 
 function App() {
+  const [headerActive, setHeaderActive] = useState(false);
+
+  useEffect(() => {
+    window.onscroll = function() {
+      showHeader();
+    };
+  } , []);
+
+  const showHeader = () => {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      setHeaderActive(true);
+    } else {
+      setHeaderActive(false);
+    }
+  }
+
   return (
     <>
+      <Header headerActive={ headerActive } />
       <Routes>
         <Route path='/' element={ <Home /> }/>
       </Routes>
---
+++ src/data.js
@@ -0,0 +1,25 @@
+const faq = [
+  {
+    question: 'Se eu esquecer algo no carro, como faz?',
+    answer: 'Você deve entrar em contato através do botão "Ajuda" disponível na viagem em que esqueceu o item, está disponível na aba "Corridas" do seu aplicativo.',
+  },
+  {
+    question: 'Se eu esquecer algo no carro, como faz?',
+    answer: 'Você deve entrar em contato através do botão "Ajuda" disponível na viagem em que esqueceu o item, está disponível na aba "Corridas" do seu aplicativo.',
+  },
+  {
+    question: 'Se eu esquecer algo no carro, como faz?',
+    answer: 'Você deve entrar em contato através do botão "Ajuda" disponível na viagem em que esqueceu o item, está disponível na aba "Corridas" do seu aplicativo.',
+  },
+  {
+    question: 'Se eu esquecer algo no carro, como faz?',
+    answer: 'Você deve entrar em contato através do botão "Ajuda" disponível na viagem em que esqueceu o item, está disponível na aba "Corridas" do seu aplicativo.',
+  },
+  {
+    question: 'Se eu esquecer algo no carro, como faz?',
+    answer: 'Você deve entrar em contato através do botão "Ajuda" disponível na viagem em que esqueceu o item, está disponível na aba "Corridas" do seu aplicativo.',
+  }
+
+]
+
+export default faq;
