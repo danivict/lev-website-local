25b9e0a235e82ab85e476ebc5394b63fbdf090ff
Caio Mendes <cacammoreno@gmail.com>
2022-06-01 21:58:11 -0300

    header responsivo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,46 +1,57 @@
 import React from 'react';
 import styles from '../styles/Footer.module.css';
 import logoFooter from '../images/logo-footer.svg';
-import appstoreImg from '../images/appstore.svg'
-import googleplayImg from '../images/googleplay.svg'
-
+import appstoreImg from '../images/appstore.svg';
+import googleplayImg from '../images/googleplay.svg';
 
 function Footer() {
-    const {footerSectionTitle, storeSection, lev} = styles;
+  const {
+    footerSectionTitle,
+    storeSection,
+    lev,
+    logo,
+    contatos,
+    app,
+    seja,
+    social,
+    copyright, 
+  } = styles;
 
-    return(
-        <footer>
-            <img src={ logoFooter } alt="Lev footer" />
-            <div>
-                <p className={ footerSectionTitle }>Contatos</p>
-                <p>Ajuda</p>
-                <a href="mailto:contato.lev@gmail.com">contato.lev@gmail.com</a>
-            </div>
-            <div>
-                <p className={ footerSectionTitle }>Baixe o app agora!</p>
-                <div className={ storeSection }>
-                    <img src={ appstoreImg } alt="appstore" />
-                    <img src={ googleplayImg } alt="googleplay" />
-                </div>
-            </div>
-            <div>
-                <p className={ footerSectionTitle }>Seja motorista Lev!</p>
-                <a className={ lev } href="#">Quero ser motorista lev</a>
-            </div>
-            <div>
-                <p>Termos de Uso e Privacidade</p>
-                <p>© 2022 lev brasil</p>
-            </div>
-            <div></div>
-            <div></div>
-            <div>
-                <p className={ footerSectionTitle }>Acompanhe a Lev nas redes sociaisAcompanhe a Lev nas redes sociais</p>
-                <i className="bi bi-facebook"></i>
-                <i className="bi bi-instagram"></i>
-                <i className="bi bi-linkedin"></i>
-            </div>
-        </footer>
-    );
+  return (
+    <footer>
+      <img src={logoFooter} className={ logo } alt='Lev footer' />
+      <div className={ contatos }>
+        <p className={footerSectionTitle}>Contatos</p>
+        <p>Ajuda</p>
+        <a href='mailto:contato.lev@gmail.com'>contato.lev@gmail.com</a>
+      </div>
+      <div className={ app }>
+        <p className={footerSectionTitle}>Baixe o app agora!</p>
+        <div className={storeSection}>
+          <img src={appstoreImg} alt='appstore' />
+          <img src={googleplayImg} alt='googleplay' />
+        </div>
+      </div>
+      <div className={ seja }>
+        <p className={footerSectionTitle}>Seja motorista Lev!</p>
+        <a className={lev} href='#'>
+          Quero ser motorista lev
+        </a>
+      </div>
+      <div className={ copyright }>
+        <p>Termos de Uso e Privacidade</p>
+        <p>© 2022 lev brasil</p>
+      </div>
+      <div className={ social }>
+        <p className={footerSectionTitle}>
+          Acompanhe a Lev nas redes sociaisAcompanhe a Lev nas redes sociais
+        </p>
+        <i className='bi bi-facebook'></i>
+        <i className='bi bi-instagram'></i>
+        <i className='bi bi-linkedin'></i>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,81 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import logo from '../images/logo.svg';
 import logoLight from '../images/logo-light.svg';
 import styles from '../styles/Header.module.css';
+import HeaderMenu from './HeaderMenu';
+import { disableScroll } from '../helpers/scrollHelper';
 
 function Header({ headerActive }) {
-  const { container, lev, active } = styles;
+  const { container, lev, active, menu } = styles;
   const navigate = useNavigate();
-  
+  const [isMobile, setIsMobile] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
+
   const logoClickHandler = () => {
     navigate('/');
-  }
+  };
+
+  useEffect(() => {
+    checkIsMobile();
+    window.addEventListener('resize', () => {
+      checkIsMobile();
+    });
+  });
+
+  const checkIsMobile = () => {
+    if (window.innerWidth <= 764) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  };
+
+  const openMenu = () => {
+    disableScroll();
+    setIsMenuOpen(true);
+  };
 
   return (
-    <header className={ `${container} ${headerActive && active}` }>
-      <img src={ headerActive? logo : logoLight } alt='Logo' onClick={ logoClickHandler }/>
-      <nav>
-        <Link to="/who">Quem somos</Link>
-        <Link to="/driver">Motorista</Link>
-        <Link to="/passenger">Passageiro</Link>
-        <Link to="/security">Segurança</Link>
-        <Link to="/help">Ajuda</Link>
-        <Link to="/lev" className={ lev }>Quero ser motorista Lev</Link>
-      </nav>
+    <header className={`${container} ${headerActive && active}`}>
+      <img
+        src={headerActive ? logo : logoLight}
+        alt='Logo'
+        onClick={logoClickHandler}
+      />
+      {/* <HeaderMenu open setIsMenuOpen={ setIsMenuOpen } lev={ lev } /> */}
+      {isMobile ? (
+        <>
+          <HeaderMenu
+            open={isMenuOpen}
+            setIsMenuOpen={setIsMenuOpen}
+            lev={lev}
+          />
+          {isMenuOpen ? (
+            <>
+              <canvas />
+            </>
+          ) : (
+            <>
+              <i
+                className={`bi bi-list ${menu} ${headerActive && active}`}
+                onClick={openMenu}
+              />
+            </>
+          )}
+        </>
+      ) : (
+        <nav>
+          <Link to='/who'>Quem somos</Link>
+          <Link to='/driver'>Motorista</Link>
+          <Link to='/passenger'>Passageiro</Link>
+          <Link to='/security'>Segurança</Link>
+          <Link to='/help'>Ajuda</Link>
+          <Link to='/lev' className={lev}>
+            Quero ser motorista Lev
+          </Link>
+        </nav>
+      )}
     </header>
   );
 }
